2bf3283473bbe151a4508a5fc49e04faec9d4360
Kentaro Kuribayashi <kentarok@gmail.com>
2025-02-26 10:24:06 +0900

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
   title: "栗林健太郎",
   description: "作家・栗林健太郎のWebサイト",
-  icon: 'https://pbs.twimg.com/profile_images/1844054466607374336/yx5dLLEs_400x400.jpg',
+  icon: 'https://pbs.twimg.com/profile_images/1893532407988367361/5EfifO80_400x400.jpg',
 };

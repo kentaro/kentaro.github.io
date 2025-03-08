2d5698a6f9362186e5162aeb09cfccc8129fa336
Kentaro Kuribayashi <kentarok@gmail.com>
2025-03-09 00:13:58 +0900

    とりあえず全部消す

--- src/config.ts
+++
@@ -1,5 +0,0 @@
-export const siteConfig = {
-  title: "栗林健太郎",
-  description: "作家・栗林健太郎のWebサイト",
-  icon: 'https://pbs.twimg.com/profile_images/1893532407988367361/5EfifO80_400x400.jpg',
-};
--- src/types.ts
+++
@@ -1,8 +0,0 @@
-export interface Entry {
-  id: number;
-  title: string | undefined;
-  date: string;
-  type: string;
-  image: string;
-  link: string;
-}
